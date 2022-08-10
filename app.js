57e3a43ea821d994cc80ccec30f3de7ec30bbd30
Gwladys Engels <gwladys.engels@gmail.com>
2022-08-10 10:43:15 +0200

    added restart popup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection  } from './snake.js'
-import { update as updateFood, draw as drawfood } from './food.js'
+import { update as updateFood, draw as drawfood , numberOfApples} from './food.js'
 import { outsideGrid } from './grid.js'
 import { speedOfSnake as speed } from './settings.js'
 
@@ -7,13 +7,15 @@ import { speedOfSnake as speed } from './settings.js'
 let lastRenderTime = 0;
 let gameOver = false;
 const gameBoard = document.getElementById("game-board");
+const lostRestart = document.getElementById("lost")
+const lostText = document.getElementById("lostText")
+const restartButton = document.getElementById("restartButton")
+
 
 
 function main(currentTime){
   if (gameOver) {
-    if (confirm('you lost. press ok to re-start')){
-      window.location = '/'
-   }
+    showMessage();
    return
   }
 
@@ -27,7 +29,8 @@ function main(currentTime){
   lastRenderTime = currentTime;
   update()
   draw()
-};
+
+}
 
 window.requestAnimationFrame(main)
 
@@ -46,4 +49,21 @@ export function draw(){
 
 function checkDeath(){
   gameOver = snakeIntersection() || outsideGrid(getSnakeHead())
+}
+
+function showMessage(){
+  document.getElementById('score').classList.add("hide")
+  lostRestart.classList.remove("hide")
+  createText()
+  restartButton.addEventListener('click', reset)
+}
+
+function createText(){
+  lostText.innerText = `You have lost after eating ${numberOfApples} 🍎 `
+}
+
+function reset(){
+  lostRestart.classList.add("hide")
+  document.getElementById('score').classList.remove("hide")
+  window.location = '/'
 }
